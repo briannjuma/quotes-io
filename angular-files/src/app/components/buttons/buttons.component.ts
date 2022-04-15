cc7169f85701f0ca574e773d8f04d8cb863f9678
jumaBrian <njengabrianj@gmail.com>
2022-04-15 16:06:33 +0300

    chore: Wrote Vote logic

--- angular-files/src/app/components/buttons/buttons.component.ts
+++ angular-files/src/app/components/buttons/buttons.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from 'src/app/model/quote';
 
 @Component({
   selector: 'app-buttons',
   templateUrl: './buttons.component.html',
-  styleUrls: ['./buttons.component.css']
+  styleUrls: ['./buttons.component.css'],
 })
 export class ButtonsComponent implements OnInit {
+  @Input() 'quote': Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  quoteDelete(complete: boolean) {
+    this.isComplete.emit(complete);
   }
 
+  @Output() isUpVote = new EventEmitter<boolean>();
+  @Output() isDownVote = new EventEmitter<boolean>();
+
+  upVote(like: boolean) {
+    this.isUpVote.emit(like);
+  }
+  downVote(dislike: boolean) {
+    this.isDownVote.emit(dislike);
+  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }
